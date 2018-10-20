79c7c30346ce0806a739a871538d9dcdebd88536
Yury Bandarchuk <yury.bandarchuk@gmail.com>
2018-10-20 12:50:57 +0300

    Clean and Init

--- src/main/scala/ru/ifmo/rain/kokorin/common/CommonClasses.scala
+++
@@ -1,7 +0,0 @@
-package ru.ifmo.rain.kokorin.common
-
-case class Document(value: Array[Byte])
-
-case class ProofElement(value: Array[Byte])
-
-case class Digest(value: Array[Byte])
--- src/test/java/ru/ifmo/rain/kokorin/sparse_mercle_tree/TestUtils.scala
+++
@@ -1,8 +0,0 @@
-package ru.ifmo.rain.kokorin.sparse_mercle_tree
-
-object TestUtils {
-  def getMap(inds: Array[Int], docs: Array[Array[Byte]]): Map[Int, Array[Byte]] = {
-    require(inds.length == docs.length)
-    inds.zip(docs).toMap
-  }
-}
