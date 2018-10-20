40ed65c2649be68038e17dc9648db9be5738bce8
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-10-20 13:19:20 +0300

    nodes

--- src/main/kotlin/avl/Common.kt
+++ src/main/kotlin/avl/Common.kt
@@ -37,9 +37,10 @@ data class Hash(val data: ByteArray) {
     }
 }
 
-sealed class Node(open var prev: Node?)
+sealed class Node(open var prev: Node?, open var hash: Hash)
 
-data class LeafNode(val key: Int, val nextKey: Int?, val data: ByteArray, override var prev: Node?): Node(prev) {
+data class LeafNode(val key: Int, val nextKey: Int?, val data: ByteArray,
+                    override var prev: Node?, override var hash: Hash): Node(prev, hash) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -61,42 +62,33 @@ data class LeafNode(val key: Int, val nextKey: Int?, val data: ByteArray, overri
     }
 }
 
-data class TreeNode(var left: Node?, var right: Node?, var hash: ByteArray,
-                    override var prev: Node?, var rightMin: Int) : Node(prev) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as TreeNode
-
-        if (left != other.left) return false
-        if (right != other.right) return false
-        if (!Arrays.equals(hash, other.hash)) return false
-        if (prev != other.prev) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = left?.hashCode() ?: 0
-        result = 31 * result + (right?.hashCode() ?: 0)
-        result = 31 * result + Arrays.hashCode(hash)
-        result = 31 * result + (prev?.hashCode() ?: 0)
-        return result
-    }
-}
+data class TreeNode(var left: Node?, var right: Node?, override var prev: Node?,
+                    var rightMin: Int, override var hash: Hash, var leftHeight: Int,
+                    var rightHeight: Int) : Node(prev, hash)
 
 val Hasher: MessageDigest = MessageDigest.getInstance("SHA-256")
 
 fun intToByteArray(x: Int): ByteArray {
     val result = ByteArray(4)
-    result[0] =
+    val x0 = (x shr 24) and 255
+    result[0] = x0.toByte()
+
+    val x1 = (x shr 16) and 255
+    result[1] = x1.toByte()
+
+    val x2 = (x shr 8) and 255
+    result[2] = x2.toByte()
+
+    val x3 = x and 255
+    result[3] = x3.toByte()
+
+    return result
 }
 
 fun hashLeafNode(key: Int, value: ByteArray, nextKey: Int?): Hash {
-    val keyBytes = ByteArray(4)
-
+    val keyBytes = intToByteArray(key)
+    val nextKeyBytes = intToByteArray(nextKey ?: Integer.MAX_VALUE)
+    return  Hash(Hasher.digest(keyBytes + nextKeyBytes + value))
 }
 
 
