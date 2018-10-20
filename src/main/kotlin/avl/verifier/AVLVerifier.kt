bd166c2c97667bf21c85c89ce12a7ded21733af2
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-10-20 14:21:37 +0300

    insertion verify

--- src/main/kotlin/avl/verifier/AVLVerifier.kt
+++ src/main/kotlin/avl/verifier/AVLVerifier.kt
@@ -1,13 +1,27 @@
 package avl.verifier
 
-import avl.Digest
-import avl.LeafData
-import avl.Proof
-import avl.hashLeafNode
+import avl.*
 
 class AVLVerifier(startDigest: Digest) {
+
+    private var curDigest = startDigest
+
     fun proofSearch(key: Int, value: LeafData, nextKey: Int?, proof: Proof): Boolean {
         var curHash = hashLeafNode(key, value, nextKey)
-        for ()
+        require(proof.entries.size == proof.directions.size)
+        val size = proof.entries.size
+        for (i in 0 until size) {
+            val treeNodeHash = proof.entries[i]
+            val curDirection = proof.directions[i]
+            curHash = if (curDirection == Direction.LEFT) {
+                hashTreeNode(curHash, treeNodeHash.data)
+            } else {
+                hashTreeNode(treeNodeHash.data, curHash)
+            }
+        }
+        return curHash.data contentEquals curDigest.data
     }
+
+    
+
 }
