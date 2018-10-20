e2d8c0753643b564ea671df5b319ac0c2709e1c7
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2018-10-20 14:01:28 +0300

    nodes

--- src/main/kotlin/avl/verifier/AVLVerifier.kt
+++ src/main/kotlin/avl/verifier/AVLVerifier.kt
@@ -1,7 +1,13 @@
 package avl.verifier
 
 import avl.Digest
+import avl.LeafData
+import avl.Proof
+import avl.hashLeafNode
 
 class AVLVerifier(startDigest: Digest) {
-
+    fun proofSearch(key: Int, value: LeafData, nextKey: Int?, proof: Proof): Boolean {
+        var curHash = hashLeafNode(key, value, nextKey)
+        for ()
+    }
 }
